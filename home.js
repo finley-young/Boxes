a9ca72b24796db59d8cc6cf8248da7ccf7fb71b0
Fin Young <125120928+finley-young@users.noreply.github.com>
2023-02-11 09:30:09 +0000

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+/*
+const container = document.querySelector(".container")
+const cloneContainer = container.cloneNode(true)
+cloneContainer.classList.add("duplicate")
+container.append(cloneContainer)
+
+container.addEventListener("click", () => {
+  container.classList.add("slide", "no-load")
+  const rows = container.querySelectorAll(".row")
+
+  const lastRow = rows[rows.length - 1]
+  lastRow.addEventListener("animationend", () => {
+    container.classList.remove("slide")
+  })
+})
+*/
